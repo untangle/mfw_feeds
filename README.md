75a02dc0710fb9c6fbfb441e49d7bc1aa310828c
Sébastien Delafond <sdelafond@gmail.com>
2018-08-17 11:11:09 +0200

    Change semantics of build.sh's -v

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ cd openwrt
 
 Build it for your intended libc target:
 ```
-./build.sh (-d x86_64|wrt3200, -l musl|glibc)
+./build.sh [-d (x86_64|wrt3200)] [-l (musl|glibc)] [-v (<branch>|<tag>|release)]
 ```
 
 The OpenWRT documentation warns that building with -jN can cause
@@ -58,7 +58,7 @@ issues. If you hit a failure with -jN the first thing to do is to rerun
 with -j1. Adding V=s increases verbosity so that you'll have output to
 look at when/if something still fails to build:
 ```
-./build.sh (-d x86_64|wrt3200, -l musl|glibc) -j1 V=s
+./build.sh [-d (x86_64|wrt3200)] [-l (musl|glibc)] [-v (<branch>|<tag>|release)] -j1 V=s
 ```
 
 Setting up a VM
--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ usage() {
   echo "Usage: $0 [-d <device>] [-l <libc>] [-v (latest|<branch>|<tag>)]"
   echo "  -d <device>              : x86_64, wrt3200, wrt1900 (defaults to x86_64)"
   echo "  -l <libc>                : musl, glibc (defaults to musl)"
-  echo "  -v latest|<branch>|<tag> : version to build from (defaults to latest)"
-  echo "                             - 'latest' is a special keyword meaning 'tip of each master branch'"
+  echo "  -v latest|<branch>|<tag> : version to build from (defaults to master)"
+  echo "                             - 'release' is a special keyword meaning 'most recent tag from each"
+  echo "                                package's source repository'"
   echo "                             - <branch> or <tag> can be any valid git object as long as it exists"
   echo "                               in each package's source repository (mfw_admin, packetd, ngfw_pkgs, etc)"
   exit 1
