f5169727ced6294b61047af2ff7a4f3cd8e0fad2
Sébastien Delafond <sdelafond@gmail.com>
2019-01-16 12:22:53 +0100

    Set initial test to ping -c 1

--- test-agent.sh
+++ test-agent.sh
@@ -8,11 +8,14 @@ MFW_HOST="mfw"
 # FIXME
 sleep 5
 
-# test SSH connectivity
-nc -z $MFW_HOST 22
+# # test SSH connectivity
+# nc -z $MFW_HOST 22
 
-# test classd connectivity
-nc -z $MFW_HOST 8123
+# # test classd connectivity
+# nc -z $MFW_HOST 8123
 
-# test webui connectivity
-echo foo | curl http://$MFW_HOST
+# # test webui connectivity
+# echo foo | curl http://$MFW_HOST
+
+ping -c 1 $MFW_HOST
+
