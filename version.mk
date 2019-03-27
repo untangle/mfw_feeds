22fa24f51f4cf92314f4d30f252a3ca95949734c
Sébastien Delafond <sdelafond@gmail.com>
2019-03-27 14:03:05 +0100

    Start on better versioning for individual packages

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 MFW_VERSION ?= $(shell git ls-remote --refs --tags $(1) | awk -F/ '!/\^\{\}$$/ {a=$$3} END {print a}')
-MFW_COMMIT = $(shell git ls-remote --refs $(1) $(2) | awk '{print substr($$1, 1, 8)}')
+MFW_COMMIT = $(shell ./mfw/git-remote-describe.sh $(1) $(2))
 
 PKG_SOURCE_VERSION := $(call MFW_VERSION,$(PKG_SOURCE_URL))
 PKG_VERSION := $(call MFW_COMMIT,$(PKG_SOURCE_URL),$(PKG_SOURCE_VERSION))
