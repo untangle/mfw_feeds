8cffd3c55c7a98bd467852dcfeb17017d707fec5
Sébastien Delafond <sdelafond@gmail.com>
2019-07-19 08:38:38 +0200

    Fixing the builder in Jenkins would be too expensive, so rename the Packages file instead
    In the future, when adding new device, we want to make sure that DEVICE
    in build.sh *exactly* matches the device name used by OpenWrt.

--- version-images.sh
+++ version-images.sh
@@ -39,6 +39,7 @@ VERSION_STRING="$(git describe --always --long --tags)_${TS}"
 case $DEVICE in
   wrt1900) DEVICE=wrt1900acs ;;
   wrt3200) DEVICE=wrt3200acm ;;
+  x86_64) DEVICE=x86-64 ;;
 esac
 
 PACKAGES_FILE="sdwan-${DEVICE}-Packages_${VERSION_STRING}.txt"
