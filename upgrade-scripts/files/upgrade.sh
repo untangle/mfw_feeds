21c20f5cb0641d1e77fdb32954d339be5d05d2f1
Dirk Morris <dmorris@untangle.com>
2019-07-23 10:22:43 -0700

    Temp upgrade script

--- upgrade-scripts/files/upgrade.sh
+++ upgrade-scripts/files/upgrade.sh
@@ -1,3 +1,122 @@
 #!/bin/sh
 
-exit 0
+usage() {
+    echo "Usage: $0 <options>"
+    echo "Options: "
+    echo "  -s - simulate - do not upgrade - exit 0 if upgrade available, 1 otherwise or error"
+    exit 1;
+}
+
+for f in /etc/os-release /tmp/sysinfo/board_name /etc/config/uid ; do
+    if [ ! -f $f ] ; then
+        echo "Missing $f file"
+        exit 1
+    fi
+done
+
+source /etc/os-release
+source /usr/share/libubox/jshn.sh
+
+DEVICE="`cat /tmp/sysinfo/board_name | tr -d '[ \t\r\n]'`"
+UID="`cat /etc/config/uid | tr -d '[ \t\r\n]'`"
+
+ARGS="version=${VERSION}&device=${BOARD}&uid=${UID}"
+URL="https://updates.untangle.com/mfw/v1/releases/${DEVICE}/latest?${ARGS}"
+OUTPUT="/tmp/upgrade.json"
+SIMULATE=0
+
+while getopts "s" o; do
+    case "${o}" in
+        s)
+            SIMULATE=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+
+echo "Checking for new releases... "
+
+rm -f $OUTPUT
+
+#wget -t 5 --timeout=30 -q -O $OUTPUT $URL
+#if [ $? != 0 ] ; then
+#    echo "Failed to reach upgrade server."
+#    exit 1
+#fi
+
+# FIXME Temporary fake response
+cat> $OUTPUT <<EOF
+{
+  "filename": "foobar123",
+  "version": "v0.1.0beta0-200-aaaaaaaaa",
+  "device": "linksys,shelby",
+  "url": "http://metaloft.com/fav.ico",
+  "timestamp": "1234124",
+  "images": [
+    {
+      "filename": "foobar",
+      "format": "foobar",
+      "type": "foobar",
+      "url": "http://metaloft.com/fav.ico",
+      "preferred": true,
+      "timestamp": "string",
+      "version": "string"
+    }
+  ],
+  "packages": [
+    {
+      "name": "string",
+      "version": "string",
+      "device": "string",
+      "description": "string"
+    }
+  ]
+}
+EOF
+
+json_init
+json_load_file $OUTPUT
+json_get_var NEWVER version
+json_get_var IMAGEURL url
+
+if [ -z "$NEWVER" ] ; then
+    echo "Invalid response (no version found)"
+    exit 1
+fi
+if [ -z "$IMAGEURL" ] ; then
+    echo "Invalid response (no URL found)"
+    exit 1
+fi
+
+echo
+echo "Newest  version: ${NEWVER}"
+echo "Current version: ${VERSION}"
+echo
+
+if [ "${VERSION}" == "${NEWVER}" ] ; then
+    if [ $SIMULATE == 1 ] ; then
+        # no upgrade available
+        exit 1
+    fi
+    exit 0
+fi
+
+if [ $SIMULATE == 1 ] ; then
+    echo "Upgrade available."
+    # upgrade available
+    exit 0
+fi
+
+echo "Downloading upgrade image..."
+wget -t 5 --timeout=30 -q -O /tmp/sysupgrade.img $IMAGEURL
+if [ $? != 0 ] ; then
+    echo "Failed to download image."
+    exit 1
+fi
+
+/sbin/sysupgrade /tmp/sysupgrade.img
+
+
