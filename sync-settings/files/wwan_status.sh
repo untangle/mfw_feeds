5ca71046f5bbb11c23dd7c79c71424247c833365
Brett Mastbergen <bmastbergen@untangle.com>
2019-11-07 15:11:32 -0500

    caswell-rssi-leds: Cache sim details
    These don't change after we boot, so look them up once and store
    them in a file for the UI to read when it needs to display them.
    
    MFW-771

--- sync-settings/files/wwan_status.sh
+++ sync-settings/files/wwan_status.sh
@@ -3,48 +3,11 @@
 . /usr/share/libubox/jshn.sh
 
 interface=$1
-full_path="/sys/class/net/$interface"
+sim_info_path="/tmp/wwan_sim_info.$interface"
 
-if [ ! -z "${interface##wwan*}" ] ; then
+if [ -f $sim_info_path  ] ; then
+	cat $sim_info_path
+else
 	echo {}
-	return
 fi
-
-if [ ! -L $full_path ] ; then
-	echo {}
-	return
-fi
-
-device="/dev/$(ls /sys/class/net/$interface/device/usbmisc)"
-
-imei=$(uqmi -s -d $device --get-imei | cut -d "\"" -f 2)
-msisdn=$(uqmi -s -d $device --get-msisdn  | cut -d "\"" -f 2)
-data_status=$(uqmi -s -d $device --get-data-status | cut -d "\"" -f 2)
-iccid=$(uqmi -s -d $device --get-iccid | cut -d "\"" -f 2)
-imsi=$(uqmi -s -d $device --get-imsi | cut -d "\"" -f 2)
-
-json_load "$(uqmi -s -d $device --get-serving-system)"
-json_get_vars registration plmn_mcc plmn_mnc plmn_description roaming
-
-json_load "$(uqmi -s -d $device --get-signal-info)"
-json_get_vars type rssi rsrq rsrp snr
-
-json_init
-json_add_string registration $registration
-json_add_int plmn_mcc $plmn_mcc
-json_add_int plmn_mnc $plmn_mnc
-json_add_string plmn_description $plmn_description
-json_add_boolean roaming $roaming
-json_add_string type $type
-json_add_int rssi $rssi
-json_add_int rsrq $rsrq
-json_add_int rsrp $rsrp
-json_add_int snr $snr
-json_add_string data_status $data_status
-json_add_string iccid $iccid
-json_add_string imsi $imsi
-json_add_string imei $imei
-json_add_string msisdn $msisdn
-json_close_object
-echo "$(json_dump)"
 
