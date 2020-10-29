c45848708f491d281d140461e02f2dbc87917a0a
Sébastien Delafond <sdelafond@gmail.com>
2020-10-29 14:33:35 +0100

    git-remote-describe: use ls-remote instead of cloning locally
    In the grand scheme of things, cloning locally before using "describe
    --always" yields more readable objects, especially when they refer to
    recent tags.
    
    However, classd is *extremely* costly to clone (a couple of minutes on
    average), and that's a huge painpoint when building clean.
    
    When we also consider the fact that there has not been *one* annotated
    tag in any of the "satellite" repositories (those referenced in
    mfw_feeds) since MFW started as a product, we can conclude that a bare
    commit hash seems to be enough to identify versions of components in our
    feeds. Therefore ls-remote provides a more efficient implementation for
    retrieving those versions.

--- git-remote-describe.sh
+++ git-remote-describe.sh
@@ -5,12 +5,4 @@ set -x
 REPO=$1
 COMMITISH=$2
 
-tmpDir=$(mktemp -d)
-
-git clone -q $REPO ${tmpDir}
-
-pushd ${tmpDir} > /dev/null
-git describe --always origin/$COMMITISH
-popd > /dev/null
-
-rm -fr ${tmpDir}
+git ls-remote --refs $REPO $COMMITISH | awk '{print $1}'
