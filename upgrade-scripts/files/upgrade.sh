f5ce5bc35acaab78967a695126bf0a7b05e21eb4
John Sommerville <jsommerville@untangle.com>
2021-01-14 13:27:15 -0700

    enhance logging so that upgrade.sh logs and exceptions are shown in logd

--- upgrade-scripts/files/upgrade.sh
+++ upgrade-scripts/files/upgrade.sh
@@ -7,9 +7,21 @@ usage() {
     exit 1;
 }
 
+# logMessage is used to log a normal stdout message to the logger
+logMessage() {
+    echo $1
+    logger -t upgrade.sh $1
+}
+
+# logException is used to log a user.crit error to the logger
+logException() {
+    echo $1
+    logger -p 2 -s -t upgrade.sh $1
+}
+
 for f in /etc/os-release /tmp/sysinfo/board_name /etc/config/uid ; do
     if [ ! -f $f ] ; then
-        echo "Missing $f file"
+        logException "Missing $f file"
         exit 1
     fi
 done
@@ -28,6 +40,16 @@ fi
 UID="`cat /etc/config/uid | tr -d '[ \t\r\n]'`"
 DEVICE="`grep DEVICE_MANUFACTURER_URL /etc/os-release | sed -rn 's/.*sdwan-(.*?)-Packages.*/\1/p'`"
 
+# ash doesn't let us use ${!var} to determine variable values, so we need to do
+# this eval echo hack to find out if the value of the arg is non-null
+for ARG in VERSION FULL_VERSION BOARD UID DEVICE ; do
+    VAL=$(eval echo \$$ARG)
+    if [ -z "$VAL" ] ; then
+        logException "Missing param for updates.untangle.com API call:  $ARG"
+        exit 1
+    fi
+done
+
 ARGS="version=${VERSION}&fullVersion=${FULL_VERSION}&board=${BOARD}&uid=${UID}"
 URL="https://updates.untangle.com/api/v1/releases/${DEVICE}/latest?${ARGS}"
 OUTPUT="/tmp/upgrade.json"
@@ -45,13 +67,13 @@ while getopts "s" o; do
 done
 
 
-echo "Checking for new releases with URL: ${URL}"
+logMessage "Checking for new releases with URL: ${URL}"
 
 rm -f $OUTPUT
 
 wget -t 5 --timeout=30 -q -O $OUTPUT $URL
 if [ $? != 0 ] ; then
-    echo "Failed to reach upgrade server."
+    logException "Failed to reach upgrade server."
     exit 1
 fi
 
@@ -85,8 +107,9 @@ fi
 # }
 # EOF
 
-if [ -z "$OUTPUT" ] ; then
+if [ -z "$OUTPUT" ] || [ ! -s "$OUTPUT" ] ; then
     # no upgrade available
+    logMessage "No upgrades available"
     exit 1
 fi
 
@@ -96,34 +119,34 @@ json_get_var NEWVER version
 json_get_var IMAGEURL url
 
 if [ -z "$NEWVER" ] ; then
-    echo "Invalid response (no version found)"
+    logException "Invalid response (no version found)"
     exit 1
 fi
 if [ -z "$IMAGEURL" ] ; then
-    echo "Invalid response (no URL found)"
+    logException "Invalid response (no URL found)"
     exit 1
 fi
 
-echo
-echo "Current version: ${VERSION}"
-echo "Newest  version: ${NEWVER}"
-echo
+logMessage
+logMessage "Current version: ${VERSION}"
+logMessage "Newest  version: ${NEWVER}"
+logMessage
 
 if [ $SIMULATE == 1 ] ; then
-    echo "Upgrade available."
+    logMessage "Upgrade available."
     # upgrade available
     exit 0
 fi
 
-echo "Downloading upgrade image..."
+logMessage "Downloading upgrade image..."
 wget -t 5 --timeout=30 -q -O /tmp/sysupgrade.img $IMAGEURL
 if [ $? != 0 ] ; then
-    echo "Failed to download image."
+    logException "Failed to download image."
     exit 1
 fi
 
-echo "Upgrading..."
-echo
+logMessage "Upgrading..."
+logMessage
 
 /sbin/sysupgrade /tmp/sysupgrade.img
 
