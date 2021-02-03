1e475925552ec00573101311a7ce65176ea055af
Christian Rasmussen <crasmussen@untangle.com>
2021-02-03 10:54:59 -0700

    Minor changes.

--- version.mk
+++ version.mk
@@ -1,7 +1,7 @@
 MFW_VERSION ?= $(shell git ls-remote --refs --tags $(1) | tail -n 1 | sed -e 's|.*/||')
 MFW_COMMIT = $(shell git ls-remote --refs $(1) refs/heads/$(2) refs/tags/$(2) | cut -f 1)
 
-PKG_SOURCE_VERSION := $(call MFW_VERSION,$(PKG_SOURCE_URL))
+PKG_SOURCE_VERSION ?= $(call MFW_VERSION,$(PKG_SOURCE_URL))
 PKG_VERSION := $(call MFW_COMMIT,$(PKG_SOURCE_URL),$(PKG_SOURCE_VERSION))
 
 PKG_SOURCE_SUBDIR := $(PKG_NAME)-$(PKG_VERSION)
