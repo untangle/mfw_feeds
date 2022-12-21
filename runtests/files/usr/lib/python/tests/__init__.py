5d1100357d2341807ac71ddab6a2a80a56302b9c
Chris Blaise <cblaise@untangle.com>
2022-12-21 12:51:39 -0500

    MFW-1542 Add new library classes and rework to import as their class names.

--- runtests/files/usr/lib/python/tests/__init__.py
+++ runtests/files/usr/lib/python/tests/__init__.py
@@ -1,2 +1,6 @@
 """Tests"""
-import tests.mfwtestcase
+
+from .lib_commands import LibCommands
+from .lib_firewall import LibFirewall
+from .lib_utilities import LibUtilities
+from .mfwtestcase import MFWTestCase
