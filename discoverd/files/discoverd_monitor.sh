f2cb55770ff2659e89a09fdea1b6eb923d52a79f
Christian Rasmussen <crasmussen@arista.com>
2023-02-21 17:11:49 -0700

    Fix operand

--- discoverd/files/discoverd_monitor.sh
+++ discoverd/files/discoverd_monitor.sh
@@ -2,7 +2,7 @@
 
 while true; do
     lsof=$(lsof | grep discoverd | grep netlink | wc -l)
-    if [[ "$lsof" -eq "100" ]]; then
+    if [[ "$lsof" -gt "100" ]]; then
         touch /tmp/discoverd_netlink_leak
         pid=$(pgrep discoverd)
         kill -QUIT $pid
