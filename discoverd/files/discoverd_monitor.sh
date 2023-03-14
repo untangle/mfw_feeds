ab199dbcb8e2dcdd20e029e6b8b1155265cd98d6
Christian Rasmussen <crasmussen@arista.com>
2023-03-14 15:21:39 -0600

    MFW-2625-rel-5.0

---
+++ discoverd/files/discoverd_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true; do
+    lsof=$(lsof | grep discoverd | grep netlink | wc -l)
+    if [[ "$lsof" -gt "100" ]]; then
+        touch /tmp/discoverd_netlink_leak
+        pid=$(pgrep discoverd)
+        kill -QUIT $pid
+        sleep 5
+        # Restart discoverd
+        /etc/init.d/discoverd restart
+    fi
+    sleep 1800
+done
