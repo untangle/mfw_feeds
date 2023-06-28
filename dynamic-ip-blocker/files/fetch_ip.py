e251aad5496cacfc3d59acdf41fa1315ea0f533d
aabhyankar1 <aabhyankar@arista.com>
2023-06-27 18:08:46 -0600

    restructuring ip-blocker

---
+++ dynamic-ip-blocker/files/fetch_ip.py
@@ -0,0 +1,38 @@
+import requests
+import ipaddress
+
+class FetchIp:
+    def is_valid_ip(ip):
+        try:
+            ipaddress.ip_address(ip)
+            return True
+        except ValueError:
+            return False
+
+    def fetch_ips_from_source(self, source_url):
+        try:
+            response = requests.get(source_url)
+            ip_addresses = []
+            if response.status_code == 200:
+                ip_addresses_response = response.text.split('\n')
+                for ip in ip_addresses_response:
+                    ip = ip.strip()
+                    if self.is_valid_ip(ip):
+                        ip_addresses.append(ip)
+                return ip_addresses
+            else:
+                print(f"Error fetching IP addresses. Status Code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching IP addresses: {e}")
+        return []
+
+    def save_ips_to_file(ip_list, file_path):
+        try:
+            file = open(file_path, 'w')
+            # for ip in ip_list:
+            #     file.write(ip + '\n')
+            file.write('\n'.join(ip_list))
+            print(f"IP addresses saved to {file_path}")
+            file.close()
+        except IOError as e:
+            print(f"Error saving IP addresses to file: {e}")
