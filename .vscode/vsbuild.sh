8a4bb020e9c8a34dba5582f8658e4e78f2b22adc
Stefanos Mitropoulos - Untangle <smitropoulos@arista.com>
2023-10-28 00:54:35 +0300

    Mfw 3831 fix vsbuild (#194)
    * fix: ls etc eos release
    
    * fix: mistype

--- .vscode/vsbuild.sh
+++ .vscode/vsbuild.sh
@@ -23,7 +23,7 @@ for target_address in "${TARGET_ADDRESSES[@]}"; do
 	ssh-copy-id root@"$target_address"
 
 	isEos=true
-	if eval [ssh -p "$PORT" root@"$target_address" "ls /etc/Eos-release"]; then
+	if ! ssh -p "$PORT" root@"$target_address" "ls /etc/Eos-release"; then
 		isEos=false
 		echo "${GREEN}Bare MFW found${NC}"
 	else
