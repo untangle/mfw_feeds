caa4b248f4450d71fe2c1dde2776aeb3f3fe7506
Stefanos Mitropoulos <smitropoulos@arista.com>
2023-12-07 00:29:47 +0200

    feat: more robust solution

--- sync-settings/files/interface_watcher.sh
+++ sync-settings/files/interface_watcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 grab_intf_name() {
-	interfaces=$(uci show network | grep $1 | cut -d"." -f2)
+	interfaces=$(uci show network | grep "$1" | cut -d"." -f2)
 }
 
 # Monitor ip link status for interfaces.
@@ -9,15 +9,36 @@ ip -o monitor link | while read -r index interface status remaining; do
 	iface=$(printf '%s\n' "$interface" | sed -E 's/(@.*)?:$//')
 	operstate=$(printf '%s\n' "$remaining" | grep -Eo ' state [^ ]+' | sed 's/^ state //')
 
-	# If iface goes to UP
+	# | tr '[:upper:]' '[:lower:]' does not work on busybox atm
+	grab_intf_name "$iface"
 	if [ "$operstate" = "UP" ]; then
-		# For both ipv6 and ipv4
-		grab_intf_name "$iface"
-		echo "$interfaces" | while read -r line; do ifup "$line"; done
-
+		action="up"
+	elif [ "$operstate" = "DOWN" ]; then
+		action="down"
+	else
+		logger -p Error -t "Interface Watch" "Unknown operating state: $operstate"
+		continue
 	fi
 
-	logger -t "Interface Watch" "Interface $iface changed state to $operstate"
+	[ "$operstate" = "UP" ] && ubus call network reload
+
+	# For both ipv6 and ipv4
+	echo "$interfaces" | while read -r intfc; do
+		# if interface empty string
+		if [ -z "$intfc" ]; then
+			continue
+		fi
+
+		# Check intfc actually exists
+		ubus -S list "network.interface.$intfc" >/dev/null || {
+			logger -p Debug -t "Interface Watch" "Interface $intfc not found in UBUS networks"
+			continue
+		}
+
+		ubus call network.interface $action "{ \"interface\" : \"$intfc\" }"
+		logger -p Info -t "Interface Watch" "Interface $intfc of device $interface changed state to $operstate"
+	done
+
 done
 
 exit 1
