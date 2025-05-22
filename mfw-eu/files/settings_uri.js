dfef6bff5f5314c64626fd6c0104eca3eec17734
acooke-arista <acooke@arista.com>
2025-05-21 22:21:43 -0600

    Updating untangle.com references

--- mfw-eu/files/settings_uri.js
+++ mfw-eu/files/settings_uri.js
@@ -1,22 +1,22 @@
 {
     "uris": {
         "uriTranslations": [{
-            "uri": "https://queue.untangle.com/v1/put",
+            "uri": "https://queue.edge.arista.com/v1/put",
             "host": "queue-eu.untangle.com"
         },{
-            "uri": "https://www.untangle.com/store/open.php",
+            "uri": "https://www.edge.arista.com/store/open.php",
             "host": "eu.untangle.com"
         },{
-            "uri": "https://www.untangle.com/cmd/networks",
+            "uri": "https://www.edge.arista.com/cmd/networks",
             "host": "eu.untangle.com"
         },{
-            "uri": "https://www.untangle.com/cmd/account/",
+            "uri": "https://www.arista.com/cmd/account/",
             "host": "eu.untangle.com"
         },{
-            "uri": "https://boxbackup.untangle.com/boxbackup/backup.php",
+            "uri": "https://boxbackup.edge.arista.com/boxbackup/backup.php",
             "host": "boxbackup-eu.untangle.com"
         },{
-            "uri": "https://cmd.untangle.com/",
+            "uri": "https://cmd.edge.arista.com/",
             "host": "cmd-eu.untangle.com"
         },{
             "uri": "https://launchpad.edge.arista.com",
